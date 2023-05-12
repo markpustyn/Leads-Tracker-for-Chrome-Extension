6a84edf879822e0fc8f96531840355099b0ddedb
markpustyn <108764244+markpustyn@users.noreply.github.com>
2023-05-12 15:20:21 -0700

    Leads tracker Updated

---
+++ README.md
@@ -0,0 +1,21 @@
+# LEADS-TRACKER JS FOR CHROME
+ Leads Tracker
+
+This is a simple Chrome extension designed to track leads by saving URLs. It has the ability to save URLs by pasting them into the extension popup and pressing the "Save" button. It also allows for deleting all saved URLs by double-clicking the delete all button.
+Usage
+
+To use the Leads Tracker, follow these steps:
+
+    Clone the repository to your local machine or download and extract the ZIP file.
+    Open Google Chrome and go to the Extensions page by typing chrome://extensions/ in the address bar.
+    Turn on Developer Mode by clicking on the toggle switch in the upper right corner of the page.
+    Click on the "Load unpacked" button and select the folder where you cloned or extracted the repository.
+    The Leads Tracker extension should now be installed and ready to use.
+    To save a URL, click on the extension icon in the toolbar to open the popup, paste the URL into the text box, and press the "Save" button.
+    To view all saved URLs, click on the extension icon in the toolbar to open the popup, and click on the "View Leads" button. This will display a list of all saved URLs.
+    To delete a single URL, hover over the URL in the list and click on the "Delete" button.
+    To delete all saved URLs, double-click on the "Delete All" button.
+
+Contributing
+
+If you find any issues or have suggestions for improvement, please feel free to contribute to the project by creating a pull request or opening an issue.
---
+++ index.js
@@ -0,0 +1,53 @@
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage);
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads)
+    })
+})
+
+
+function renderLeads(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+    console.log("double clicked")
+    localStorage.clear()
+    myLeads = []
+    renderLeads(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads(myLeads);
+})
